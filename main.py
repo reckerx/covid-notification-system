c9220f7d70fc1a382d41bf2b9eed2fd5239f187a
reckerx <40579178+reckerx@users.noreply.github.com>
2021-08-14 17:12:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from plyer import notification
 import requests
-import time
+
 
 
 def notifyMe(title, message):
@@ -18,26 +18,29 @@ def getData(url):
 
 
 if __name__ == "__main__":
-        url = "https://www.mohfw.gov.in/data/datanew.json"
-        data = requests.get(url).json()
+    url = "https://www.mohfw.gov.in/data/datanew.json"
+    data = requests.get(url).json()
 
-        my_dict = {}
-        for dict in data:
-            my_dict[dict["state_name"]] = dict
+    my_dict = {}
+    for dict in data:
+        my_dict[dict["state_name"]] = dict
 
-        v = []
+    v = []
 
-        def print_info(state):
-            info = my_dict[state]
-            val = list(info.values())
-            v.append(val[1])
-            v.append(val[6])
-            v.append(val[8])
-            v.append(val[9])
+    def print_info(state):
+        info = my_dict[state]
+        val = list(info.values())
+        v.append(val[1])
+        v.append(val[6])
+        v.append(val[8])
+        v.append(val[9])
 
-        print_info("Odisha")  # here we can put ant states name
+    print_info("Odisha")  # here we can put ant states name
 
-        nTitle = "Cases of Covid-19"
-        nText = f"State : {v[0]}\nActive : {v[1]}\nCured : {v[2]}\nDeath : {v[3]}"
-        notifyMe(nTitle, nText)
+    nTitle = "Cases of Covid-19"
+    nText = f"State : {v[0]}\nActive : {v[1]}\nCured : {v[2]}\nDeath : {v[3]}"
+    notifyMe(nTitle, nText)
+
+
+
 
